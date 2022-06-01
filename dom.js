11e42809ff17bfd604a1a055b77c3f1c4696e70d
maykaltenev <michaeltenev@gmail.com>
2022-06-01 13:21:51 +0200

    selectors

--- dom.js
+++ dom.js
@@ -11,13 +11,54 @@ console.log(document.links);
 console.log(document.images);
 
 // GET ELEMENTS BY ID
-console.log(document.getElementById('header-title'));
-let headerTitle = document.getElementById('header-title');
-let header = document.getElementById('main-header');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerText = 'Goodbye';
-headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom = 'solid 3px #000'
+// console.log(document.getElementById('header-title'));
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom = 'solid 3px #000'
 
-//GET ELEMENTS BY CLASSNAME
+// GET ELEMENTS BY CLASSNAME
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items)
+// console.log(items[1])
+// items[1].textContent = 'Hello 2';
+// items[1].style.background = 'yellow';
+
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.background = '#f4f4f4'
+// }
+
+// QuerrySelector 
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = 'SEND';
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+
+// Querryselectorall
+var titles = document.querySelectorAll('.title')
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}
